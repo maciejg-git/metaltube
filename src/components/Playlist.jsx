a71922fad418de7a5cce7afb62c3fdc2531743a2
maciejg-git <m.gralek@gmail.com>
2026-06-29 02:16:03 +0200

    update

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -3,11 +3,13 @@ import BHandThumbsUp from "../icons/BHandThumbsUp";
 import BPlayFill from "../icons/BPlayFill";
 import BPauseFill from "../icons/BPauseFill";
 import clsx from "clsx";
+import { useState } from "react";
 
-const ytimgUrl = "https://i.ytimg.com/vi"
+const ytimgUrl = "https://i.ytimg.com/vi";
 
 const Playlist = ({ data, playerId, playerState, onImageClick, layout }) => {
-  const PlaylistItemComponent = layout === "normal" ? PlaylistItem : layout === "compact" ? PlaylistItemCompact : PlaylistItem
+  const PlaylistItemComponent =
+    layout === "normal" ? PlaylistItem : layout === "compact" ? PlaylistItemCompact : PlaylistItem;
 
   return (
     <div className="flex flex-col divide-y divide-gray-200 dark:divide-neutral-700">
@@ -48,12 +50,36 @@ const ImageCover = ({ item, onImageClick, playingItem, layout }) => {
         )}
       >
         {playingItem ? (
-          <div className={clsx("rounded-full bg-black/50", layout === "normal" && "p-2", layout === "compact" && "p-1")}>
-            <BPauseFill className={clsx("text-gray-100", layout === "normal" && "h-16 w-16", layout === "compact" && "h-10 w-10")}></BPauseFill>
+          <div
+            className={clsx(
+              "rounded-full bg-black/50",
+              layout === "normal" && "p-2",
+              layout === "compact" && "p-1",
+            )}
+          >
+            <BPauseFill
+              className={clsx(
+                "text-gray-100",
+                layout === "normal" && "h-16 w-16",
+                layout === "compact" && "h-10 w-10",
+              )}
+            ></BPauseFill>
           </div>
         ) : (
-          <div className={clsx("rounded-full bg-black/50", layout === "normal" && "p-2", layout === "compact" && "p-1")}>
-            <BPlayFill className={clsx("text-gray-100", layout === "normal" && "h-16 w-16 translate-x-1", layout === "compact" && "h-10 w-10 translate-x-0.5")}></BPlayFill>
+          <div
+            className={clsx(
+              "rounded-full bg-black/50",
+              layout === "normal" && "p-2",
+              layout === "compact" && "p-1",
+            )}
+          >
+            <BPlayFill
+              className={clsx(
+                "text-gray-100",
+                layout === "normal" && "h-16 w-16 translate-x-1",
+                layout === "compact" && "h-10 w-10 translate-x-0.5",
+              )}
+            ></BPlayFill>
           </div>
         )}
       </div>
@@ -62,35 +88,55 @@ const ImageCover = ({ item, onImageClick, playingItem, layout }) => {
 };
 
 const PlaylistItem = ({ item, playerId, playerState, onImageClick }) => {
+  const [similarBands, setSimilarBands] = useState()
+
   const playingItem = playerId === item.id && playerState === 2;
+
+  let handleClickSimilar = async () => {
+    let res = await fetch(`/.netlify/functions/get-similar-bands?band=${item.band}`);
+    res = await res.json();
+    setSimilarBands(res)
+  };
 
   return (
     <div className="flex gap-x-4 py-4">
-      <ImageCover item={item} onImageClick={onImageClick} playingItem={playingItem} layout="normal" />
-      <div className="group flex flex-1 flex-col gap-y-1">
-        <div className="flex items-center text-xl font-semibold">
-          <a href={`https://www.youtube.com/watch?v=${item.id}`} target="_blank">
-            {item.title}
-          </a>
+      <ImageCover
+        item={item}
+        onImageClick={onImageClick}
+        playingItem={playingItem}
+        layout="normal"
+      />
+
+      <div className="group flex flex-1 flex-col">
+        <div className="flex flex-1">
+          <div className="group flex flex-1 flex-col gap-y-1">
+            <div className="flex items-center text-xl font-semibold">
+              <a href={`https://www.youtube.com/watch?v=${item.id}`} target="_blank">
+                {item.title}
+              </a>
+            </div>
+            <div className="text-lg">{item.displayGenre || item.genre}</div>
+            <div className="text-gray-600 dark:text-gray-400">{item.country}</div>
+            <div className="text-gray-600 dark:text-gray-400">{item.year}</div>
+          </div>
+          <div className="flex flex-col items-start gap-x-4 md:flex-row">
+            <div className="flex items-center gap-x-1">
+              <BEye></BEye>
+              {item.views}
+            </div>
+            <div className="flex items-center gap-x-1">
+              <BHandThumbsUp></BHandThumbsUp>
+              {item.likes}
+            </div>
+          </div>
+        </div>
+        <div className="ml-auto hidden group-hover:block">
           <a
             href={`https://www.metal-archives.com/bands/${item.band}/`}
-            className="ml-4 hidden text-sm text-sky-500 underline group-hover:block dark:text-sky-400"
+            className="ml-4 text-sm text-sky-500 underline dark:text-sky-400 font-semibold"
           >
             metal-archives
           </a>
-        </div>
-        <div className="text-lg">{item.displayGenre || item.genre}</div>
-        <div className="text-gray-600 dark:text-gray-400">{item.country}</div>
-        <div className="text-gray-600 dark:text-gray-400">{item.year}</div>
-      </div>
-      <div className="flex flex-col md:flex-row items-start gap-x-4">
-        <div className="flex items-center gap-x-1">
-          <BEye></BEye>
-          {item.views}
-        </div>
-        <div className="flex items-center gap-x-1">
-          <BHandThumbsUp></BHandThumbsUp>
-          {item.likes}
         </div>
       </div>
     </div>
@@ -102,33 +148,42 @@ const PlaylistItemCompact = ({ item, playerId, playerState, onImageClick }) => {
 
   return (
     <div className="flex gap-x-4 py-2">
-      <ImageCover item={item} onImageClick={onImageClick} playingItem={playingItem} layout="compact" />
-      <div className="group flex flex-1 flex-col gap-y-1">
-        <div className="flex items-center font-semibold">
-          <a href={`https://www.youtube.com/watch?v=${item.id}`} target="_blank">
-            {item.title}
-          </a>
+      <ImageCover
+        item={item}
+        onImageClick={onImageClick}
+        playingItem={playingItem}
+        layout="compact"
+      />
+      <div className="group flex flex-1">
+        <div className="flex flex-1 flex-col gap-y-1">
+          <div className="flex items-center font-semibold">
+            <a href={`https://www.youtube.com/watch?v=${item.id}`} target="_blank">
+              {item.title}
+            </a>
+          </div>
+          <div className="text-base">{item.displayGenre || item.genre}</div>
+          <div className="flex gap-x-2 text-sm">
+            <div className="text-gray-600 dark:text-gray-400">{item.country}</div>
+            <div className="text-gray-600 dark:text-gray-400">{item.year}</div>
+          </div>
+        </div>
+        <div className="flex flex-col items-end justify-between">
+          <div className="flex flex-col items-start gap-x-4 text-sm md:flex-row">
+            <div className="flex items-center gap-x-1">
+              <BEye></BEye>
+              {item.views}
+            </div>
+            <div className="flex items-center gap-x-1">
+              <BHandThumbsUp></BHandThumbsUp>
+              {item.likes}
+            </div>
+          </div>
           <a
             href={`https://www.metal-archives.com/bands/${item.band}/`}
-            className="ml-4 hidden text-sm text-sky-500 underline group-hover:block dark:text-sky-400"
+            className="hidden text-sm text-sky-500 underline group-hover:block dark:text-sky-400 font-semibold"
           >
             metal-archives
           </a>
-        </div>
-        <div className="text-base">{item.displayGenre || item.genre}</div>
-        <div className="flex gap-x-2 text-sm">
-          <div className="text-gray-600 dark:text-gray-400">{item.country}</div>
-          <div className="text-gray-600 dark:text-gray-400">{item.year}</div>
-        </div>
-      </div>
-      <div className="flex flex-col md:flex-row items-start gap-x-4 text-sm">
-        <div className="flex items-center gap-x-1">
-          <BEye></BEye>
-          {item.views}
-        </div>
-        <div className="flex items-center gap-x-1">
-          <BHandThumbsUp></BHandThumbsUp>
-          {item.likes}
         </div>
       </div>
     </div>
